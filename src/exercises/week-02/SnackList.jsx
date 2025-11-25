7ca734506e05c9431752d4ec1ef767a709b09c21
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-25 15:31:00 -0500

    feat: added sort on ranking. Appears done

--- src/exercises/week-02/SnackList.jsx
+++ src/exercises/week-02/SnackList.jsx
@@ -1,14 +1,19 @@
-const snackList = [
-  { name: 'DumDum Suckers', rank: 3 },
-  { name: 'RedVine licorice', rank: 2 },
-  { name: 'Cinnamon Powdered Donuts', rank: 1 },
-];
-
 export default function SnackList() {
+  let snackList = [
+    { name: 'DumDum Suckers', rank: 3 },
+    { name: 'RedVine licorice', rank: 2 },
+    { name: 'Cinnamon Powdered Donuts', rank: 1 },
+  ];
+
+  //each varible is an obj
+  snackList.sort((a, b) => a.rank - b.rank);
+
+  console.log(snackList);
+
   return (
     <ul>
       {snackList.map((snack) => {
-        return <li key={`${snack.name[0]}_${snack.rank}`}>{snack}</li>;
+        return <li key={`${snack.name[0]}_${snack.rank}`}>{snack.name}</li>;
       })}
     </ul>
   );
